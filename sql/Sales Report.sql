6b39ffdc11b81a60dab3d50d5b7b1055a7cd32ed
Rocky Manganye <NtshuxekoM@hollywoodbets.net>
2024-03-18 20:13:14 +0200

    update storedProc

--- sql/Sales Report.sql
+++ sql/Sales Report.sql
@@ -1,17 +1,17 @@
 use Ecommerce;
-
-CREATE PROCEDURE SearchProducts
-   DECLARE @PreviousMonthsCoun INT
+GO 
+Create PROCEDURE dbo.SearchProducts
+     @PreviousMonthsCount INT
 AS
 BEGIN 
-	DECLARE @SearchDate DATETIME = DATEADD(MONTH, -@PreviousMonthsCoun, GETDATE());
+  DECLARE @SearchDate DATETIME = DATEADD(MONTH, -@PreviousMonthsCount, GETDATE());
 
-	SELECT TOP 10 p.Id, p.Name , SUM([Count])  'Purchased Count' , COUNT(c.Id) 'Unique Customers' FROM dbo.Product p
+	SELECT TOP 10 p.Id, p.Name , SUM([Count])  'Quantity Sold' , COUNT(c.Id) 'No. Unique Customers' FROM dbo.Product p
 	INNER JOIN dbo.ProductBaskets pb ON p.Id = pb.ProductId
 	INNER JOIN dbo.Basket bsk on bsk.Id = pb.BasketId
 	INNER JOIN dbo.Customer c on c.id = bsk.CustomerId
 	where pb.DateCreated >= @SearchDate
 	Group By p.Name , pb.DateCreated ,p.Id
-	Order BY pb.DateCreated asc
-
+	Order BY pb.DateCreated
 END
+GO
